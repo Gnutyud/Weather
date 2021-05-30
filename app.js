162d9e0d1a0c72f46e1d77de1f8e1efbec057b6a
Gnutyud <nguyenduytungtd3@gmail.com>
2021-05-31 00:24:37 +0700

    created

---
+++ app.js
@@ -0,0 +1,198 @@
+const weather = {
+  current: {
+    location: "",
+    temp: "",
+    description: "",
+    icon: "",
+  },
+  hourly: [
+    {
+      hour: "",
+      icon: "",
+      temp: "",
+    },
+    {
+      hour: "",
+      icon: "",
+      temp: "",
+    },
+    {
+      hour: "",
+      icon: "",
+      temp: "",
+    },
+    {
+      hour: "",
+      icon: "",
+      temp: "",
+    },
+    {
+      hour: "",
+      icon: "",
+      temp: "",
+    },
+    {
+      hour: "",
+      icon: "",
+      temp: "",
+    },
+  ],
+  daily: [
+    {
+      date: "",
+      icon: "",
+      temp_hight: "",
+      temp_low: "",
+    },
+    {
+      date: "",
+      icon: "",
+      temp_hight: "",
+      temp_low: "",
+    },
+    {
+      date: "",
+      icon: "",
+      temp_hight: "",
+      temp_low: "",
+    },
+    {
+      date: "",
+      icon: "",
+      temp_hight: "",
+      temp_low: "",
+    },
+    {
+      date: "",
+      icon: "",
+      temp_hight: "",
+      temp_low: "",
+    },
+    {
+      date: "",
+      icon: "",
+      temp_hight: "",
+      temp_low: "",
+    },
+    {
+      date: "",
+      icon: "",
+      temp_hight: "",
+      temp_low: "",
+    },
+    {
+      date: "",
+      icon: "",
+      temp_hight: "",
+      temp_low: "",
+    },
+  ],
+};
+const nav_time = document.getElementById("nav_time");
+const location_city = document.getElementsByClassName("location_city")[0];
+const weather_desc = document.getElementsByClassName("weather_desc")[0];
+const current_icon = document.getElementById("current_icon");
+const current_temp = document.getElementsByClassName("current_temp")[0];
+const content_hourly = document.getElementsByClassName("content_hourly")[0];
+var content_daily = document.getElementsByClassName("content_daily")[0];
+setInterval(() => {
+  let dayOrNight = "AM";
+  let d = new Date();
+  let h = d.getHours();
+  let m = d.getMinutes();
+  h = checkTime(h);
+  m = checkTime(m);
+  if (h >= 12) {
+    dayOrNight = "PM";
+  }
+
+  function checkTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+  nav_time.innerHTML = `${h}:${m} ${dayOrNight}`;
+}, 1000);
+// Get geographic coordinates
+const notification = document.getElementById("notification");
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(showPosition, showError);
+} else {
+  notification.innerHTML =
+    "<p>Geolocation is not supported by this browser.</p>";
+}
+function showPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  getWeather(lat, lon);
+}
+function showError(error) {
+  notification.innerHTML = ` <p>${error.message}</p> `;
+}
+// Call API
+const key = "0e1684e64e0188293e167670f42a897e";
+function getWeather(lat, lon) {
+  let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then(function (data) {
+      weather.current.location = data.timezone;
+      weather.current.temp = Math.floor(data.current.temp - 273);
+      weather.current.description = data.current.weather[0].description;
+      weather.current.icon = data.current.weather[0].icon;
+      // hourly
+      for (let i = 0; i < weather.hourly.length; i++) {
+        // unix time so need to times 1000 to milliseconds
+        weather.hourly[i].hour = (
+          "0" + new Date(data.hourly[i].dt * 1000).getHours()
+        ).substr(-2);
+        weather.hourly[i].icon = data.hourly[i].weather[0].icon;
+        weather.hourly[i].temp = Math.floor(data.hourly[i].temp - 273);
+      }
+      // Daily
+      for (let i = 0; i < 8; i++) {
+        // unix time so need to times 1000 to milliseconds
+        weather.daily[i].date = new Date(data.daily[i].dt * 1000)
+          .toDateString()
+          .substr(0, 4);
+        weather.daily[i].icon = data.daily[i].weather[0].icon;
+        weather.daily[i].temp_hight = Math.floor(data.daily[i].temp.max - 273);
+        weather.daily[i].temp_low = Math.floor(data.daily[i].temp.min - 273);
+      }
+      // console.log(weather.daily);
+    })
+    .then(function displayWeather() {
+      location_city.innerHTML = weather.current.location;
+      weather_desc.innerHTML = weather.current.description;
+      current_temp.innerHTML = weather.current.temp + "&#176";
+      current_icon.setAttribute("src", `icons/${weather.current.icon}.png`);
+      // For hourly section
+
+      let hourly_html = "";
+      weather.hourly.forEach(function (hourly) {
+        hourly_html += `
+        <div class="hourly-item">
+                    <p class="title">${hourly.hour}</p>
+                    <img src="icons/${hourly.icon}.png">
+                    <p class="hourly_temp">${hourly.temp}&#176</p>
+                </div>`;
+        content_hourly.innerHTML = hourly_html;
+      });
+      // For daily section
+      let daily_html = "";
+      // weather.daily.forEach((day) => console.log(day));
+      for (let i = 0; i < weather.daily.length; i++) {
+        daily_html += `
+                <div class="daily-item">
+                    <p class="title">${weather.daily[i].date}</p>
+                    <img src="icons/${weather.daily[i].icon}.png">
+                    <p class="low_high">
+                        <span>H:${weather.daily[i].temp_hight}&#176</span>
+                        <span>L:${weather.daily[i].temp_low}&#176</span>
+                    </p>
+                </div>`;
+        content_daily.innerHTML = daily_html;
+      }
+    });
+}
